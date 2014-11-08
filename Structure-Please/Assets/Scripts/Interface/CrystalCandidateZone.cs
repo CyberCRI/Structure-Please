2240fc61c72d572e863c27b8e639b927ae4bab80
raphael goujet <raphael.goujet@gmail.com>
2014-11-08 15:31:28 +0100

    first commit on interface - pressing A changes the character being displayed

---
+++ Structure-Please/Assets/Scripts/Interface/CrystalCandidateZone.cs
@@ -0,0 +1,53 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class CrystalCandidateZone : MonoBehaviour {
+
+
+	public SpriteRenderer candidate;
+
+	public Sprite sprite1;
+	public Sprite sprite2;
+	public Sprite sprite3;
+
+	private float _timeSinceLastPressed = 0;
+	private float _timeBetweenTwoPressed = 1;
+	private int _currentIndex = 1;
+
+	public void displayNextCrystal(int i)
+	{
+		Debug.Log("display "+i);
+		candidate.gameObject.SetActive(true);
+		//candidate.guiTexture = new GUITexture();
+
+		switch(i)
+		{
+		case 1:
+			candidate.sprite = sprite1;
+			break;
+		case 2:
+			candidate.sprite = sprite2;
+			break;
+		case 3:
+			candidate.sprite = sprite3;
+			break;
+		}
+	}
+
+	// Use this for initialization
+	void Start () {
+
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		_timeSinceLastPressed += Time.deltaTime;
+		if(Input.GetKey(KeyCode.A) && (_timeSinceLastPressed > _timeBetweenTwoPressed))
+		{
+			_currentIndex = (_currentIndex+1) %3 + 1;
+			Debug.Log(_currentIndex);
+			displayNextCrystal(_currentIndex);
+			_timeSinceLastPressed = 0;
+		}
+	}
+}
