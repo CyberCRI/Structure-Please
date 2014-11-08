6a6f1377bb2d2e25a478dd96e780ad9d8dd5e9d3
Jesse Himmelstein <jesse@heavenstone.net>
2014-11-08 21:54:44 +0100

    Added additional card panels

---
+++ Structure-Please/Assets/Scripts/Interface/ColorCardPanel.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class ColorCardPanel : MonoBehaviour {
+
+	public GUIText colorText;
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+	public void display(Crystal testResults)
+	{
+		colorText.text = testResults.color;
+	}
+}
